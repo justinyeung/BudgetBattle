42bec9489cc476813a7ac5cb8e1f1066bc5536c8
Justin <justingcyeung@gmail.com>
2020-04-22 22:11:59 -0400

    add simple fb auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
+    <div>
+      <a href="http://localhost:5000/api/auth/login/facebook/">Login to Facebook</a>
+      {/* <div id="fb-root">
+
+      </div>
+      <div className="fb-login-button" 
+        data-size="large" 
+        data-button-type="continue_with" 
+        data-layout="default" 
+        data-auto-logout-link="false" 
+        data-use-continue-as="false" 
+        data-width="">
+
+      </div> */}
     </div>
   );
 }
+
 
 export default App;
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,54 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
 const User = require('../models/User');
+
+passport.use(new Strategy({
+    clientID: '611749673018879',
+    clientSecret: 'ceaf0b4e762bb3dc27191d1819ecd80d',
+    callbackURL: 'http://localhost:3000/api/auth/callback'
+    },
+    function(accessToken, refreshToken, profile, cb){
+        let user = User.findOne({ email: profile.emails[0].value });
+        // console.log(user);
+        if(!user){
+            user = new User({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                password: 'testing'
+            });
+            user.save();
+        }else{
+            return cb(err, user);
+        };
+        return cb(null, profile);
+}));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+// after authenticating
+router.get('/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+// log in
+router.get('/login/facebook',
+  passport.authenticate('facebook')
+);
+
+
 
 module.exports = router;
