8faf6bcf07c0e863268b13e1527b70f5592a7a85
Justin <justingcyeung@gmail.com>
2020-05-14 01:01:41 -0400

    add expansion panel for competitions

--- client/src/components/competitions/CurrentComps.js
+++ client/src/components/competitions/CurrentComps.js
@@ -3,6 +3,50 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getAcceptedComp } from '../../actions/competitionActions';
 
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontWeight: "fontWeightBold"
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  winningColour: {
+      color: 'green'
+  },
+  icon: {
+    verticalAlign: 'bottom',
+    height: 20,
+    width: 20,
+  },
+  details: {
+    alignItems: 'center',
+    paddingTop: '16px'
+  },
+  detailsAvatar: {
+    alignItems: 'center',
+    paddingTop: '32px'
+  },
+  column: {
+    flexBasis: '25%',
+  }
+}));
+
 const CurrentComps = ({ getAcceptedComp, competition: { accepted } }) => {
 
     useEffect(() => {
@@ -12,33 +56,82 @@ const CurrentComps = ({ getAcceptedComp, competition: { accepted } }) => {
         // eslint-disable-next-line
     }, []);
 
+    const classes = useStyles();
+
     return (
         <div>
             <h1>Competitions:</h1>
-            <ul>
-                {accepted !== [] && 
-                    accepted.map(comp => (
-                        <div key={comp._id}>
-                            <li>
-                                <ul>
-                                    <li>
-                                        Competition ID: {comp._id}
-                                    </li>
-                                    <li>
-                                        User1: {comp.user1}
-                                    </li>
-                                    <li>
-                                        User2: {comp.user2}
-                                    </li>
-                                    <li>
-                                        Status: {comp.status}
-                                    </li>
-                                </ul>
-                            </li>
-                        </div>
-                    ))
-                }
-            </ul>
+            <Container maxWidth='lg'>
+                <div className={classes.root}>
+                    {accepted !== [] && 
+                        accepted.map(comp => (
+                            <ExpansionPanel defaultExpanded key={comp._id}>
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1c-content"
+                                    id="panel1c-header"
+                                    >
+                                    <div id="competitions-left-column" className={classes.column}>
+                                        <Typography id='competitor-header-name' variant='h5'>Alyssa Schleifer</Typography>
+                                    </div>
+                                    <div id="competitions-center-column" className={classes.column} />
+                                    <div id="competitions-right-column" className={classes.column}>
+                                        <Typography className={classes.secondaryHeading}>CompetitionID: {comp._id}</Typography>
+                                    </div>
+                                </ExpansionPanelSummary>
+                                <Divider />
+                                <ExpansionPanelDetails className={classes.detailsAvatar}>
+                                    <div id="competitions-left-column" className={classes.column}>
+                                        <Avatar>JY</Avatar>
+                                    </div>
+                                    <div id="competitions-center-column" className={classes.column} />
+                                    <div id="competitions-right-column" className={classes.column}>
+                                        <Avatar>AS</Avatar>
+                                    </div>
+                                </ExpansionPanelDetails>
+                                <ExpansionPanelDetails className={classes.details}>
+                                    <div id="competitions-left-column" className={classes.column}>
+                                        <Typography variant='h5'>Justin Yeung</Typography>
+                                    </div>
+                                    <div id="competitions-center-column" className={classes.column} />
+                                    <div id="competitions-right-column" className={classes.column}>
+                                        <Typography variant='h5'>Alyssa Schleifer</Typography>
+                                    </div>
+                                </ExpansionPanelDetails>
+                                <ExpansionPanelDetails className={classes.details}>
+                                    <div id="competitions-left-column" className={classes.column}>
+                                        <Typography variant='h5'>$145</Typography>
+                                    </div>
+                                    <div id="competitions-center-column" className={classes.column}>
+                                        <h5>Total Spent</h5>
+                                    </div>
+                                    <div id="competitions-right-column" className={classes.column}>
+                                        <Typography variant='h5'>$160</Typography>
+                                    </div>
+                                </ExpansionPanelDetails>
+                                <ExpansionPanelDetails className={classes.details}>
+                                    <div id="competitions-left-column" className={classes.column}>
+                                        <Typography className={classes.winningColour} variant='h5'>-15</Typography>
+                                    </div>
+                                    <div id="competitions-center-column" className={classes.column}>
+                                        <h5>Difference</h5>
+                                    </div>
+                                    <div id="competitions-right-column" className={classes.column}>
+                                        <Typography color='error' variant='h5'>+15</Typography>
+                                    </div>
+                                </ExpansionPanelDetails>
+                                <Divider />
+                                <ExpansionPanelActions>
+                                    <Button size="small" color="primary">
+                                        View More
+                                    </Button>
+                                </ExpansionPanelActions>
+                            </ExpansionPanel>
+
+                        ))
+                    }
+                </div>
+            </Container>
         </div>
     )
 }
