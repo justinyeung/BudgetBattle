4acf16864ba9ee35804e54f3427339da14d0414e
Justin <justingcyeung@gmail.com>
2020-06-12 00:00:46 -0400

    add loading to login buttons

--- client/src/components/auth/GGLoginBtn.js
+++ client/src/components/auth/GGLoginBtn.js
@@ -1,15 +1,25 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { GoogleLoginButton } from "react-social-login-buttons";
+import { setUserLoading } from "../../actions/userActions";
 
-const GGLoginBtn = () => {
+const GGLoginBtn = ({ setUserLoading }) => {
   return (
     <div>
-      <a id="login-link" href="http://localhost:5000/api/ggauth/login/">
+      <a
+        id="login-link"
+        href="http://localhost:5000/api/ggauth/login/"
+        onClick={setUserLoading}
+      >
         <GoogleLoginButton />
       </a>
     </div>
   );
 };
 
-export default GGLoginBtn;
+GGLoginBtn.propTypes = {
+  setUserLoading: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setUserLoading })(GGLoginBtn);
