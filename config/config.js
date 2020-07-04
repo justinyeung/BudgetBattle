7eab5c7c610db51145e30646b2bced18a0af2841
Justin <justingcyeung@gmail.com>
2020-07-04 00:44:33 -0400

    remove local db uri

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const DB_PROD_URI = process.env.REACT_APP_DB_URI;
-const DB_DEV_URI = process.env.REACT_APP_LOCAL_DB_URI;
 
 module.exports = {
     mongodburi: DB_PROD_URI,
